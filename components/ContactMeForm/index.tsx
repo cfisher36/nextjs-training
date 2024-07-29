cee197dafc5b18ee0fcef734b32f870f1a0bb883
Fisher <caleb.fisher@cgi.com>
2024-07-29 14:56:41 -0400

    adds error messages to contact me form

--- components/ContactMeForm/index.tsx
+++ components/ContactMeForm/index.tsx
@@ -56,34 +56,46 @@ const ContactMeForm: React.FC = () => {
         <form onSubmit={form.handleSubmit(processForm)}>
             <div className="font-bold">Contact Me</div>
             <div>
-                <label htmlFor="name">Name:</label>
+                <label htmlFor="name" className="block">Name:</label>
                 <input
                     type="text"
                     id="name"
                     {...form.register("name")}
                     className="base-input responsive-text"
                 />
+                {form.formState.errors.name && (
+                    <p className="text-red-500 text-sm mt-1">{form.formState.errors.name.message}</p>
+                )}
             </div>
             <div>
-                <label htmlFor="email">Email:</label>
+                <label htmlFor="email" className="block">Email:</label>
                 <input
                     type="email"
                     id="email"
                     {...form.register("email")}
                     className="base-input responsive-text"
                 />
+                {form.formState.errors.email && (
+                    <p className="text-red-500 text-sm mt-1">{form.formState.errors.email.message}</p>
+                )}
             </div>
             <div>
-                <label htmlFor="message">Message:</label>
+                <label htmlFor="message" className="block">Message:</label>
                 <textarea
                     id="message"
                     {...form.register("message")}
-                    className="w-full rounded-sm border-0 p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  focus:ring-sky-600 sm:text-sm sm:leading-6"
+                    className="w-full rounded-sm border-0 p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                    rows={3}
                 />
+                {form.formState.errors.message && (
+                    <p className="text-red-500 text-sm">{form.formState.errors.message.message}</p>
+                )}
             </div>
             <div>{message}</div>
+            <div className="py-1">
             <button type="submit" className="bg-green-500 hover:bg-green-300 text-white font-bold py-1 px-4 rounded mr-2">Save</button>
             <button type="button" onClick={resetForm} className="bg-red-600 hover:bg-red-300 text-white font-bold py-1 px-4 rounded">Reset</button>
+            </div>
         </form>
     )
 }
