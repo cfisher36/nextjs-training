8212efdfde4d425d94b4a5bc8169daf0250c0cf6
Fisher <caleb.fisher@cgi.com>
2024-07-30 09:51:15 -0400

    moves suspense for fetch page out of loading.tsx

--- app/fetch/page.tsx
+++ app/fetch/page.tsx
@@ -1,5 +1,14 @@
 import CatViewer from "@/components/CatViewer";
+import LoadingSkeleton from "@/components/LoadingSkeleton";
+import { Suspense } from "react";
 
 export default function Fetch() {
-  return <CatViewer />;
+  return (
+    <main className="min-h-screen p-6 sm:p-8 md:p-12 lg:p-24">
+      <h1 className="flex justify-start text-2xl sm:text-xl font-bold mb-4">The Daily Cat</h1>
+      <Suspense fallback={<LoadingSkeleton />}>
+        <CatViewer />
+      </Suspense>
+    </main>
+  );
 }
